93ea0fe97e1c4d99aa7a5241b5390b30e96edb80
Stefano Perazzolo <stefanoperazzolo@Stefanos-MacBook-Pro.local>
2021-02-23 10:40:20 +0100

    PIN-25: Define models completed PIN-26: Define OpenApi file completed

--- src/main/scala/it/pagopa/pdnd/interop/uservice/partyregistryproxy/api/impl/OrganizationApiServiceImpl.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/partyregistryproxy/api/impl/OrganizationApiServiceImpl.scala
@@ -3,20 +3,27 @@ package it.pagopa.pdnd.interop.uservice.partyregistryproxy.api.impl
 import akka.http.scaladsl.marshalling.ToEntityMarshaller
 import akka.http.scaladsl.server.Route
 import it.pagopa.pdnd.interop.uservice.partyregistryproxy.api.OrganizationApiService
+import it.pagopa.pdnd.interop.uservice.partyregistryproxy.model
 import it.pagopa.pdnd.interop.uservice.partyregistryproxy.model.Organization
 
 class OrganizationApiServiceImpl extends OrganizationApiService {
-
   override def getOrganizationById200(responseOrganization: Organization)(implicit
     toEntityMarshallerOrganization: ToEntityMarshaller[Organization]
-  ): Route = ???
+  ): Route = super.getOrganizationById200(responseOrganization)
 
-  override def getOrganizationById400: Route = ???
+  override def getOrganizationById400(responseError: model.Error)(implicit
+    toEntityMarshallerError: ToEntityMarshaller[model.Error]
+  ): Route = super.getOrganizationById400(responseError)
 
-  override def getOrganizationById404: Route = ???
+  override def getOrganizationById404(responseError: model.Error)(implicit
+    toEntityMarshallerError: ToEntityMarshaller[model.Error]
+  ): Route = super.getOrganizationById404(responseError)
 
   override def getOrganizationById(orgId: String)(implicit
-    toEntityMarshallerOrganization: ToEntityMarshaller[Organization]
-  ): Route = getOrganizationById200(Organization("id", "dn", "description", "pec"))
+    toEntityMarshallerOrganization: ToEntityMarshaller[Organization],
+    toEntityMarshallerError: ToEntityMarshaller[model.Error]
+  ): Route = {
+    getOrganizationById200(Organization("id", "dn", "description", "pec"))
+  }
 
 }
