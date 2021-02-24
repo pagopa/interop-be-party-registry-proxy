0e391eccee47fbf25d408239cca18aa55b62752a
Stefano Perazzolo <stefanoperazzolo@Stefanos-MacBook-Pro.local>
2021-02-24 11:50:37 +0100

    PN-41: improve openapi file

--- src/main/scala/it/pagopa/pdnd/interop/uservice/partyregistryproxy/api/impl/PublicApiMarshallerImpl.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/partyregistryproxy/api/impl/PublicApiMarshallerImpl.scala
@@ -2,11 +2,16 @@ package it.pagopa.pdnd.interop.uservice.partyregistryproxy.api.impl
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.marshalling.ToEntityMarshaller
-import it.pagopa.pdnd.interop.uservice.partyregistryproxy.api.OrganizationApiMarshaller
-import it.pagopa.pdnd.interop.uservice.partyregistryproxy.model.{OrganizationError, Organization}
+import it.pagopa.pdnd.interop.uservice.partyregistryproxy.api.PublicApiMarshaller
+import it.pagopa.pdnd.interop.uservice.partyregistryproxy.model.{Organization, OrganizationError, Status}
 import spray.json._
 
-class OrganizationApiMarshallerImpl extends OrganizationApiMarshaller with SprayJsonSupport with DefaultJsonProtocol {
+class OrganizationApiMarshallerImpl extends PublicApiMarshaller with SprayJsonSupport with DefaultJsonProtocol {
+
+  override implicit def toEntityMarshallerStatus: ToEntityMarshaller[Status] = sprayJsonMarshaller(
+    jsonFormat1(Status.apply)
+  )
+
   override implicit def toEntityMarshallerOrganization: ToEntityMarshaller[Organization] = sprayJsonMarshaller(
     jsonFormat4(Organization.apply)
   )
--- src/main/scala/it/pagopa/pdnd/interop/uservice/partyregistryproxy/api/impl/PublicApiServiceImpl.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/partyregistryproxy/api/impl/PublicApiServiceImpl.scala
@@ -2,10 +2,17 @@ package it.pagopa.pdnd.interop.uservice.partyregistryproxy.api.impl
 
 import akka.http.scaladsl.marshalling.ToEntityMarshaller
 import akka.http.scaladsl.server.Route
-import it.pagopa.pdnd.interop.uservice.partyregistryproxy.api.OrganizationApiService
-import it.pagopa.pdnd.interop.uservice.partyregistryproxy.model.{Organization, OrganizationError}
+import it.pagopa.pdnd.interop.uservice.partyregistryproxy.api.PublicApiService
+import it.pagopa.pdnd.interop.uservice.partyregistryproxy.model.{Organization, OrganizationError, Status}
 
-class OrganizationApiServiceImpl extends OrganizationApiService {
+class OrganizationApiServiceImpl extends PublicApiService {
+
+  override def getStatus200(responseStatus: Status)(implicit
+    toEntityMarshallerStatus: ToEntityMarshaller[Status]
+  ): Route = ???
+
+  override def getStatus()(implicit toEntityMarshallerStatus: ToEntityMarshaller[Status]): Route = ???
+
   override def getOrganizationById200(responseOrganization: Organization)(implicit
     toEntityMarshallerOrganization: ToEntityMarshaller[Organization]
   ): Route = ???
