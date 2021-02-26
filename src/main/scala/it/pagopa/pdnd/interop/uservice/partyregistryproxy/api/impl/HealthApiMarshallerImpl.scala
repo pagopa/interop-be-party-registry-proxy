e19ec5ae119d133c5c5d2c0b88d22336cf9e988b
Stefano Perazzolo <stefanoperazzolo@Stefanos-MacBook-Pro.local>
2021-02-26 10:35:59 +0100

    PN-41: Italian API Guidelines passed

--- src/main/scala/it/pagopa/pdnd/interop/uservice/partyregistryproxy/api/impl/HealthApiMarshallerImpl.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/partyregistryproxy/api/impl/HealthApiMarshallerImpl.scala
@@ -3,11 +3,31 @@ package it.pagopa.pdnd.interop.uservice.partyregistryproxy.api.impl
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.marshalling.ToEntityMarshaller
 import it.pagopa.pdnd.interop.uservice.partyregistryproxy.api.HealthApiMarshaller
-import it.pagopa.pdnd.interop.uservice.partyregistryproxy.model.Status
+import it.pagopa.pdnd.interop.uservice.partyregistryproxy.model.Problem
 import spray.json._
 
+import java.net.URI
+import scala.util.{Failure, Success, Try}
+
 class HealthApiMarshallerImpl extends HealthApiMarshaller with SprayJsonSupport with DefaultJsonProtocol {
-  override implicit def toEntityMarshallerStatus: ToEntityMarshaller[Status] = sprayJsonMarshaller(
-    jsonFormat1(Status.apply)
+
+  implicit val uriFormat: JsonFormat[URI] =
+    new JsonFormat[URI] {
+      override def write(obj: URI): JsValue = JsString(obj.toString)
+
+      override def read(json: JsValue): URI = json match {
+        case JsString(s) =>
+          Try(URI.create(s)) match {
+            case Success(result) => result
+            case Failure(exception) =>
+              deserializationError(s"could not parse $s as Joda LocalDateTime", exception)
+          }
+        case notAJsString =>
+          deserializationError(s"expected a String but got a ${notAJsString.compactPrint}")
+      }
+    }
+
+  override implicit def toEntityMarshallerProblem: ToEntityMarshaller[Problem] = sprayJsonMarshaller(
+    jsonFormat5(Problem.apply)
   )
 }
--- src/main/scala/it/pagopa/pdnd/interop/uservice/partyregistryproxy/api/impl/HealthApiServiceImpl.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/partyregistryproxy/api/impl/HealthApiServiceImpl.scala
@@ -3,14 +3,12 @@ package it.pagopa.pdnd.interop.uservice.partyregistryproxy.api.impl
 import akka.http.scaladsl.marshalling.ToEntityMarshaller
 import akka.http.scaladsl.server.Route
 import it.pagopa.pdnd.interop.uservice.partyregistryproxy.api.HealthApiService
-import it.pagopa.pdnd.interop.uservice.partyregistryproxy.model.Status
+import it.pagopa.pdnd.interop.uservice.partyregistryproxy.model.Problem
 
 class HealthApiServiceImpl extends HealthApiService {
-
-  override def getStatus200(responseStatus: Status)(implicit
-    toEntityMarshallerStatus: ToEntityMarshaller[Status]
+  override def getStatus200(responseProblem: Problem)(implicit
+    toEntityMarshallerProblem: ToEntityMarshaller[Problem]
   ): Route = ???
 
-  override def getStatus()(implicit toEntityMarshallerStatus: ToEntityMarshaller[Status]): Route = ???
-
+  override def getStatus()(implicit toEntityMarshallerProblem: ToEntityMarshaller[Problem]): Route = ???
 }
