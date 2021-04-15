7a7346ddfb421223e39b61e8007da39d11248a33
Stefano Perazzolo <stefanoperazzolo@ip-192-168-1-232.eu-central-1.compute.internal>
2021-04-15 19:19:11 +0200

    upgrade libs

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,8 +1,8 @@
 object Versions {
-  lazy val akkaVersion        = "2.6.13"
+  lazy val akkaVersion        = "2.6.14"
   lazy val akkaHttpVersion    = "10.2.4"
   lazy val vaultDriverVersion = "5.1.0"
-  lazy val luceneVersion      = "8.8.1"
+  lazy val luceneVersion      = "8.8.2"
   lazy val openapi4jVersion   = "1.0.7"
   lazy val kamonVersion       = "2.1.15"
   lazy val logbackVersion     = "1.2.3"
