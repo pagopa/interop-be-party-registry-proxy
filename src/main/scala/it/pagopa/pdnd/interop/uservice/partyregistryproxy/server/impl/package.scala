0c786556d7859c9d87ab09f9ede575c3a7019a53
Stefano Perazzolo <stefanoperazzolo@ip-192-168-1-232.eu-central-1.compute.internal>
2021-05-24 11:39:55 +0200

    utc time

--- src/main/scala/it/pagopa/pdnd/interop/uservice/partyregistryproxy/server/impl/package.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/partyregistryproxy/server/impl/package.scala
@@ -3,16 +3,16 @@ package it.pagopa.pdnd.interop.uservice.partyregistryproxy.server
 import com.bettercloud.vault.{SslConfig, Vault, VaultConfig}
 import com.typesafe.config.{Config, ConfigFactory}
 
-import java.time.{LocalDate, LocalDateTime, LocalTime}
+import java.time.{LocalDate, LocalDateTime, LocalTime, ZoneId}
 import java.time.temporal.ChronoUnit
 
 package object impl {
 
   def getInitialDelay(cronTime: String): Long = {
     val startTime: LocalTime = LocalTime.parse(cronTime)
-    val today: LocalDate     = LocalDate.now()
+    val today: LocalDate     = LocalDate.now(ZoneId.of("UTC"))
     val next: LocalDateTime  = LocalDateTime.of(today, startTime)
-    val now: LocalDateTime   = LocalDateTime.now()
+    val now: LocalDateTime   = LocalDateTime.now(ZoneId.of("UTC"))
     val diff: Long           = now.until(next, ChronoUnit.MILLIS)
     if (diff < 0) Math.abs(diff) else diff
   }
