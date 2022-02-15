e436c41af27ea5d524b06dfacd1c700b4887dc1c
Stefano Perazzolo <stefano.perazzolo@pagopa.it>
2022-02-15 11:26:15 +0100

    refactoring

--- src/main/scala/it/pagopa/pdnd/interop/uservice/partyregistryproxy/common/system/ApplicationConfiguration.scala
+++ src/main/scala/it/pagopa/pdnd/interop/uservice/partyregistryproxy/common/system/ApplicationConfiguration.scala
@@ -7,13 +7,11 @@ object ApplicationConfiguration {
 
   lazy val serverPort: Int = config.getInt("party-registry-proxy.port")
 
-  lazy val institutionsIndexDir: String = config.getString("party-registry-proxy.index.institutions.folder")
-  lazy val institutionsIpaOpenDataUrl: String =
-    config.getString("party-registry-proxy.ipa.institutions.open-data-url")
+  lazy val institutionsIndexDir: String       = config.getString("party-registry-proxy.index.institutions.folder")
+  lazy val institutionsIpaOpenDataUrl: String = config.getString("party-registry-proxy.ipa.institutions.open-data-url")
 
-  lazy val categoriesIndexDir: String = config.getString("party-registry-proxy.index.categories.folder")
-  lazy val categoriesIpaOpenDataUrl: String =
-    config.getString("party-registry-proxy.ipa.categories.open-data-url")
+  lazy val categoriesIndexDir: String       = config.getString("party-registry-proxy.index.categories.folder")
+  lazy val categoriesIpaOpenDataUrl: String = config.getString("party-registry-proxy.ipa.categories.open-data-url")
 
   lazy val ipaOrigin: String = config.getString("party-registry-proxy.ipa.origin")
 }
