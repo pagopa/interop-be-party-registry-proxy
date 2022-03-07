a7225625eba72f2349ed91f553c3c7f8551bec0b
beetlecrunch <stefano.perazzolo@gmail.com>
2022-03-07 11:45:31 +0100

    fix InstitutionOps: Origin field

--- src/main/scala/it/pagopa/interop/partyregistryproxy/service/impl/package.scala
+++ src/main/scala/it/pagopa/interop/partyregistryproxy/service/impl/package.scala
@@ -73,7 +73,7 @@ package object impl {
       doc.add(new TextField(InstitutionField.DIGITAL_ADDRESS.value, institution.digitalAddress, Field.Store.YES))
       doc.add(new TextField(InstitutionField.ADDRESS.value, institution.address, Field.Store.YES))
       doc.add(new TextField(InstitutionField.ZIP_CODE.value, institution.zipCode, Field.Store.YES))
-      doc.add(new TextField(InstitutionField.ORIGIN.value, ApplicationConfiguration.ipaOrigin, Field.Store.YES))
+      doc.add(new TextField(InstitutionField.ORIGIN.value, institution.origin, Field.Store.YES))
 
       doc.addOptional(InstitutionField.O.value, institution.o)
       doc.addOptional(InstitutionField.OU.value, institution.ou)
