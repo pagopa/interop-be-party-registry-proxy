24a526147f79e36c3dba2fa270f0f56fb476ae68
Scala Steward <scala-steward@pagopa.it>
2022-03-25 07:53:22 +0000

    Revert commit(s) ed6bd74

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,7 +1,7 @@
 object Versions {
   lazy val akkaHttpVersion       = "10.2.6"
   lazy val akkaManagementVersion = "1.1.1"
-  lazy val akkaVersion           = "2.6.19"
+  lazy val akkaVersion           = "2.6.17"
   lazy val json4sVersion         = "4.0.1"
   lazy val kamonVersion          = "2.2.2"
   lazy val logbackVersion        = "1.2.9"
